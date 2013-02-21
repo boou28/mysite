4230232d9d3007ad879cb057498c197572a099d4
boou28 <boou28@gmail.com>
2013-02-21 02:27:10 -0800

    Start

---
+++ javascripts/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+	$('body').hide().fadeIn(1000);
+});
+ 
+$(document).ready(function() {
+   alert('hi');
+});
+
+
+	/*	$('#name').css("background-image","url('images/a_color.png')");     use mouse enter leave, or hover over outer div to change background image back
+	
+		$(document).ready(function() {
+	$('.namelink').hover(function () {
+		$('#name').css({"background-position":"center 21px","-webkit-transition":"background-image .3s cubic-bezier(0, .5, 1, 1)","background-image":"url('images/a_color.png')"})
+		},function() {
+		$('#name').css({"-webkit-transition":"background-image .3s cubic-bezier(0, .5, 1, 1)","background-image":"url('images/a_color0g.png')"});
+		})
+});
+
+	
+	
+	
+	*/
